4df53cb4e042b09fa609b71ead53ab385ce61cfd
Douniabf <157621126+Douniabf@users.noreply.github.com>
2024-02-20 18:09:49 +0100

    20/02/2024

--- app/public/wp-content/themes/blankslate-child/functions.php
+++ app/public/wp-content/themes/blankslate-child/functions.php
@@ -1,24 +1,57 @@
 <?php
 // Exit if accessed directly
-if ( !defined( 'ABSPATH' ) ) exit;
+
+use Elementor\Core\Admin\UI\Components\Button;
+
+if (!defined('ABSPATH')) exit;
 
 // BEGIN ENQUEUE PARENT ACTION
 // AUTO GENERATED - Do not modify or remove comment markers above or below:
 
-if ( !function_exists( 'chld_thm_cfg_locale_css' ) ):
-    function chld_thm_cfg_locale_css( $uri ){
-        if ( empty( $uri ) && is_rtl() && file_exists( get_template_directory() . '/rtl.css' ) )
+if (!function_exists('chld_thm_cfg_locale_css')) :
+    function chld_thm_cfg_locale_css($uri)
+    {
+        if (empty($uri) && is_rtl() && file_exists(get_template_directory() . '/rtl.css'))
             $uri = get_template_directory_uri() . '/rtl.css';
         return $uri;
     }
 endif;
-add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
+add_filter('locale_stylesheet_uri', 'chld_thm_cfg_locale_css');
 
-if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
-    function chld_thm_cfg_parent_css() {
-        wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array(  ) );
+if (!function_exists('chld_thm_cfg_parent_css')) :
+    function chld_thm_cfg_parent_css()
+    {
+        wp_enqueue_style('chld_thm_cfg_parent', trailingslashit(get_template_directory_uri()) . 'style.css', array());
     }
 endif;
-add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
+add_action('wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10);
+
+add_filter('wp_nav_menu_items', 'add_admin_link', 10, 2);
+
+function add_admin_link($items, $args)
+{
+    if (is_user_logged_in() ) {
+        $items .= '<li><a href="' . get_admin_url() . '">Admin</a></li>';
+    }
+
+    return $items;
+}
+
+add_filter( 'wp_nav_menu_items', 'move_admin_menu_item', 10, 2 );
+function move_admin_menu_item( $items, $args ) {
+    // Vérifiez si l'utilisateur est connecté
+    if ( is_user_logged_in() ) {
+        // Trouvez la position de l'élément "Admin" dans la liste
+        $admin_position = strpos( $items, 'class="menu-item admin-item"' );
+
+        // Si l'élément "Admin" est trouvé, déplacez-le après le premier élément
+        if ( false !== $admin_position ) {
+            $items = substr_replace( $items, 'class="menu-item admin-item"', $admin_position, 0 );
+        }
+    }
+
+    return $items;
+}
+
 
 // END ENQUEUE PARENT ACTION
--- app/public/wp-content/themes/blankslate-child/header.php
+++ app/public/wp-content/themes/blankslate-child/header.php
@@ -14,6 +14,6 @@
             <a href="http://planty.local/" ><img  src="http://planty.local/wp-content/uploads/2024/01/Logo.png" alt="Logo-0planty"></a>
             <nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
                 <?php wp_nav_menu(array('theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>')); ?>
-                
+                <a href="http://planty.local/commander/"><button class="button" >Commander</button></a>
             </nav>
         </header>
